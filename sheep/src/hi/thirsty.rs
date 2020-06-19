b12de4864d0a1c41d8f84231b32a4d1d97d34101
SheepTester <ussa.say@gmail.com>
2020-06-19 16:24:22 -0700

    strings are revolutionary

--- sheep/src/hi/thirsty.rs
+++ sheep/src/hi/thirsty.rs
@@ -8,10 +8,28 @@
 /// let sigue = "a gamepro5";
 /// ```
 pub fn gutchen() -> i8 {
+    println!("{}", years_of_age::intro_ducir());
     -3
 }
 
-// mod years_of_age {
-//     /// Perro.
-//     fn sixty()
-// }
+fn privadaaaaa() -> String {
+    String::from("sity")
+}
+
+mod years_of_age {
+    struct Thirt {
+        y_one: i64,
+        six_six: String,
+    }
+
+    /// Perro.
+    fn sixty() -> Thirt {
+        return Thirt {y_one: 31, six_six: String::from("66")}
+    }
+
+    pub fn intro_ducir() -> String {
+        let eight = sixty();
+        println!("{}", super::privadaaaaa());
+        String::from(format!("thirt{}...{}", eight.y_one, eight.six_six))
+    }
+}
