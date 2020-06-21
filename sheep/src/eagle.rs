fc36a5ba714288d0b32c8b03c77c36e5e9de04cf
SheepTester <ussa.say@gmail.com>
2020-06-21 13:48:44 -0700

    wasm

--- sheep/src/eagle.rs
+++ sheep/src/eagle.rs
@@ -1,4 +1,4 @@
+pub mod gp5_perf_test;
 /// I think mod is like an import statement and tells the compiler to include the file with that
 /// name. It needs to be public or then main.rs can't access strtest.rs. Scary!
 pub mod strtest;
-pub mod gp5_perf_test;
