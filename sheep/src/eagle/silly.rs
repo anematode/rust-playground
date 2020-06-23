37f19faf6ee6dd98462d780aa0260e30f7c9476f
SheepTester <ussa.say@gmail.com>
2020-06-22 23:03:55 -0700

    so apparently you can do `break 42`

--- sheep/src/eagle/silly.rs
+++ sheep/src/eagle/silly.rs
@@ -12,6 +12,20 @@ pub fn random_int() -> i8 {
     // "^ unreachable expression"
 }
 
+/// pub fn random_int_2() -> i8 {
+///     loop {
+///         if rand::random::<u8>() == -1 {
+///             break;
+///         }
+///         return 4;
+///     }
+///     // Intellectual!
+///     // "any code following this expression is unreachable"
+///     // 6
+///     // "^ unreachable expression"
+/// }
+
 pub fn silly() {
-    println!("{}", random_int())
+    println!("{}", random_int());
+    // println!("{}", random_int_2());
 }
