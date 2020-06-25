fb0f57001d0d7f376eaf7aa0989a53eb646ec11b
SheepTester <ussa.say@gmail.com>
2020-06-24 17:36:34 -0700

    image texture :O

--- sheep_etc/webgl/src/learning.rs
+++ sheep_etc/webgl/src/learning.rs
@@ -1,17 +1,27 @@
 use glium::{Surface, glutin, implement_vertex, uniform};
-use std::fs;
+use std::io::Cursor;
 
 #[derive(Copy, Clone)]
 struct Vertex {
     position: [f32; 2],
+    tex_coords: [f32; 2],
 }
-implement_vertex!(Vertex, position);
+implement_vertex!(Vertex, position, tex_coords);
+
+fn load_image<'a>(bytes: &[u8]) -> glium::texture::RawImage2d<'a, u8> {
+    let image = image::load(
+        Cursor::new(bytes),
+        image::ImageFormat::Png,
+    ).unwrap().to_rgba();
+    let image_dimensions = image.dimensions();
+    glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions)
+}
 
 pub fn main() {
-    let vertex_shader_src = fs::read_to_string("./src/learning/vertex.vert")
-        .expect("Getting vertex shader was problematic");
-    let fragment_shader_src = fs::read_to_string("./src/learning/fragment.frag")
-        .expect("Getting fragment shader was problematic");
+    let vertex_shader_src = String::from_utf8_lossy(include_bytes!("./learning/vertex.vert")).into_owned();
+    let fragment_shader_src = String::from_utf8_lossy(include_bytes!("./learning/fragment.frag")).into_owned();
+
+    let image = load_image(&include_bytes!("./learning/texture.png")[..]);
 
     let event_loop = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new();
@@ -22,19 +32,21 @@ pub fn main() {
         &display,
         vertex_shader_src.as_str(),
         fragment_shader_src.as_str(),
-        None
+        None,
     ).unwrap();
 
     // Triangle vertices
-    let vertex1 = Vertex { position: [-0.5, -0.5] };
-    let vertex2 = Vertex { position: [ 0.0,  0.5] };
-    let vertex3 = Vertex { position: [ 0.5, -0.25] };
+    let vertex1 = Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5], tex_coords: [0.0, 1.0] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25], tex_coords: [1.0, 0.0] };
     let shape = vec![vertex1, vertex2, vertex3];
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
     // A "dummy marker" because we only have one triangle so that WebGL knows what triangles
     // can be made from these vertices
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let texture = glium::texture::Texture2d::new(&display, image).unwrap();
 
     let mut t: f32 = -0.5;
     event_loop.run(move |ev, _, control_flow| {
@@ -55,9 +67,10 @@ pub fn main() {
                     [-t.sin(), t.cos(), 0.0, 0.0],
                     [0.0, 0.0, 1.0, 0.0],
                     [0.0, 0.0, 0.0, 1.0f32],
-                ]
+                ],
+                tex: &texture,
             },
-            &Default::default()
+            &Default::default(),
         ).unwrap();
         target.finish().unwrap();
 
--- sheep_etc/webgl/src/learning/vertex.vert
+++ sheep_etc/webgl/src/learning/vertex.vert
@@ -1,12 +1,15 @@
 #version 140
 
 in vec2 position;
+in vec2 tex_coords;
 // Pass attribute to vertex shader
-out vec2 my_attr;
+// out vec2 my_attr;
+out vec2 v_tex_coords;
 
 uniform mat4 matrix;
 
 void main() {
-    my_attr = position;
+    v_tex_coords = tex_coords;
+    // my_attr = position;
     gl_Position = matrix * vec4(position, 0.0, 1.0);
 }
