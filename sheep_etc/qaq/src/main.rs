c47c45fa7768be236570e251f0ab0cfe4a080fbf
Sean <ussa.say@gmail.com>
2021-07-14 19:01:33 -0700

    for loops are cringe
    also I think this is the problem: https://codeforces.com/problemset/problem/894/A

--- sheep_etc/qaq/src/main.rs
+++ sheep_etc/qaq/src/main.rs
@@ -1,26 +1,25 @@
 use std::io::{self, Read};
 
+/// https://codeforces.com/problemset/problem/894/A
 fn main() {
     let bytes_result: Result<Vec<u8>, _> = io::stdin().bytes().collect();
     let bytes = bytes_result.expect("failed to read bytes or smth");
-    let mut count = 0;
-    for (i, maybe_a) in bytes.iter().enumerate() {
-        if *maybe_a != b'A' {
-            continue;
-        }
-        let mut left = 0;
-        let mut right = 0;
-        for maybe_q in bytes.iter().take(i) {
-            if *maybe_q == b'Q' {
-                left += 1;
-            }
-        }
-        for maybe_q in bytes.iter().skip(i + 1) {
-            if *maybe_q == b'Q' {
-                right += 1;
-            }
-        }
-        count += left * right;
-    }
-    println!("{}", count);
+    println!(
+        "{}",
+        bytes
+            .iter()
+            .enumerate()
+            .filter(|(_, maybe_a)| **maybe_a == b'A')
+            .map(|(i, _)| bytes
+                .iter()
+                .take(i)
+                .filter(|maybe_q| **maybe_q == b'Q')
+                .count()
+                * bytes
+                    .iter()
+                    .skip(i + 1)
+                    .filter(|maybe_q| **maybe_q == b'Q')
+                    .count())
+            .sum::<usize>()
+    );
 }
